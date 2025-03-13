eac6d908aec232eae35dc86ef19ee682a06566d1
Roshni Gupta <roshni.g@turing.com>
2025-03-13 16:02:32 +0530

    displaying amount

--- tip.py
+++ tip.py
@@ -2,3 +2,4 @@ bill = float(input("Enter bill amount: $"))
 tip_percent = float(input("Enter tip percentage: "))
 tip = bill * (tip_percent / 100)
 total = bill + tip
+print(f"Total amount: ${total:.2f} (Tip: ${tip:.2f})")
